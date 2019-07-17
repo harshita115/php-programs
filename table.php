8278cd5ab031ebb10645aa4b31b4267205cfdd25
etherealharshita <41101208+harshita115@users.noreply.github.com>
2019-07-17 13:10:24 +0530

    table

---
+++ table.php
@@ -0,0 +1,24 @@
+<form Method="post">
+<br><br>
+Enter Number: <input type="text" name="value" />
+<br>
+<input type="submit" name="btn" value="print table" />
+</form>
+
+<?php
+if (isset ( $_POST['btn'] )
+
+	$n=$_POST['value'];
+	$res=0;
+	for ($i=1; $i <= 10; $i++) { 
+		$res=$n*$i;
+		echo $n;
+		echo "*";
+		echo $i;
+		echo "=";
+		echo $res;
+		echo "<br>";
+	}
+
+
+?>
